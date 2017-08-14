6fdf7ef14bfa19eac204469dd09844a25712c739
kylewelt <kylewelt@umich.edu>
2017-08-14 14:55:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-function findMinAndRemove(array){
+function findMinAndRemove (array) {
+  let index = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[index]) {
+      index = i
+    }
+  }
 
+  let min = array[index]
+  array.splice(index, 1)
+  return min
 }
 
-function insertionSort(array){
+function insertionSort (array) {
+  let min
+  let sorted = []
 
+  while (array.length !== 0) {
+    min = findMinAndRemove(array)
+    sorted.push(min)
+  }
+
+  return sorted
 }
